ca59088e336d50484e91fc5a40318eafc7f21f19
tgothorp <tgothorp@users.noreply.github.com>
2026-03-06 21:20:05 +0000

    health label

--- scripts/pawns/CharacterPawn.cs
+++ scripts/pawns/CharacterPawn.cs
@@ -14,15 +14,33 @@ public partial class CharacterPawn : MoveablePawn
 
     [Export(hintString: "Team this pawn belongs to.")]
     public Team Team { get; set; }
+    
+    private Label3D _healthLabel;
+
+    public override void _EnterTree()
+    {
+        _healthLabel = GetNode<Label3D>("HealthLabel");
+
+        base._EnterTree();
+    }
 
     public override void _Ready()
     {
         CurrentHitPoints = HitPoints;
+        if (_healthLabel != null)
+        {
+            _healthLabel.Text = $"{CurrentHitPoints.ToString()}/{HitPoints}";
+        }
+        
         base._Ready();
     }
 
     public void TakeDamage(uint amount)
     {
         CurrentHitPoints -= amount;
+        if (_healthLabel != null)
+        {
+            _healthLabel.Text = $"{CurrentHitPoints.ToString()}/{HitPoints}";
+        }
     }
 }
